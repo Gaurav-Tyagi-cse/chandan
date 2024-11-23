2ed4719e8cfda79388271896fc4cee8c2a12ed34
Gaurav Tyagi <118004334+Gaurav-Tyagi-cse@users.noreply.github.com>
2024-11-23 12:05:06 +0530

    first commit

---
+++ js/bookcountryconsultation.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const popupContainer = document.getElementById('divBookConsultation');
+
+    // Load the popup content dynamically
+    fetch('/common/bookCountryConsultation.html')
+        .then(response => response.text())
+        .then(data => {
+            popupContainer.innerHTML = data;
+
+            initializePredictiveText();
+        })
+        .catch(error => console.error('Error loading popup:', error));
+});
+
+function initializePredictiveText() {
+    $(document).ready(function () {
+        // Call the function from predictive-course.js to initialize predictive text
+        if (typeof initializePredictiveAutocomplete === "function") {
+            initializePredictiveAutocomplete();
+        } else {
+            console.error("Predictive text initialization function not found.");
+        }
+    });
+}
+
